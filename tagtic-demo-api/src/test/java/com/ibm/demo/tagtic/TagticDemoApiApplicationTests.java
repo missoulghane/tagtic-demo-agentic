2c119c57042a055dc0407f1f14e84e32a14f5ba8
missoulghane <m.issoulghane@gmail.com>
2026-05-27 14:20:59 +0200

    Ajout des templates angular et springboot

---
+++ tagtic-demo-api/src/test/java/com/ibm/demo/tagtic/TagticDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.demo.tagtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagticDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
